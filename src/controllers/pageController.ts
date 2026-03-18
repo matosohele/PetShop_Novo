870e5f8c667164cef748bb00bf7f44836a59d803
Helena Matoso <helena.matoso@aluno.ifsp.edu.br>
2026-03-18 08:31:59 -0300

    criacaoMenu

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from 'express';
+import { createMenuObject } from '../helpers/createMenuObject';
+
+export const home = (req: Request, res: Response) => {
+	res.render('pages/page', {
+		menu: createMenuObject('all'),
+		banner: {
+			title: 'Todos os animais',
+			background: 'allanimals.jpg'
+		}
+	});
+};
+
+export const dogs = (req: Request, res: Response) => {
+	res.render('pages/page', {
+		menu: createMenuObject('dogs'),
+		banner: {
+			title: 'Cachorros',
+			background: 'banner_dog.jpg'
+		}
+	});
+};
+
+export const cats = (req: Request, res: Response) => {
+	res.render('pages/page', {
+		menu: createMenuObject('cats'),
+		banner: {
+			title: 'Gatos',
+			background: 'banner_cat.jpg'
+		}
+	});
+};
+
+export const fishes = (req: Request, res: Response) => {
+	res.render('pages/page', {
+		menu: createMenuObject('fishes'),
+		banner: {
+			title: 'Peixes',
+			background: 'banner_fish.jpg'
+		}
+	});
+};
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
-import path from 'path'
+import path from 'path';
 import mainRouter from './routes/index';
 
 dotenv.config();
@@ -16,4 +16,8 @@ server.use(express.static(path.join(__dirname, '../public')));
 
 server.use(mainRouter);
 
-server.listen(process.env.PORT)
+server.use((req, res) => {
+	res.send('404 - Not Found');
+});
+
+server.listen(process.env.PORT);
